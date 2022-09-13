32ece6f869a1a6c9b95b2cf4e8996781d2324805
Ricardo Hitalo Araujo Soares <ricardohitalo1@gmail.com>
2022-09-13 16:09:35 -0300

    add-> footer in game cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url(/background-galaxy.png)",
-        'nlw-gradient':'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)',
+        'nlw-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
