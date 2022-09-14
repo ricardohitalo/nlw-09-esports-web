c63bfced1c462853577683d1ffd31d795e67b7cc
Ricardo Hitalo Araujo Soares <ricardohitalo1@gmail.com>
2022-09-14 10:41:51 -0300

    add-> card ads duo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
 import './styles/main.css'
 
 import logoImg from './assets/logo-nlw-esports.svg'
@@ -60,6 +61,20 @@ function App() {
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
         </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2a3624] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos Playes!</span>
+          </div>
+            <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size="24" />
+              Publicar anúncio
+            </button>
+        </div>
+
       </div>
     </div>
   )
